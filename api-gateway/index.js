510f1d315035422b8976642327ddc6957ade8795
Lucas-Tales1 <lucastales100@gmail.com>
2025-11-21 22:06:36 -0300

    fix: corrigir erros de CORS e favicon

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,42 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-
-const { getHistorico, postJogada } = require("./services/restClient");
-const { jogarSOAP } = require("./services/soapClient");
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// Habilita CORS
 app.use(cors());
-app.use(express.json());
 
-// Endpoints REST
-app.get("/rest/historico", async (req, res) => {
-  try {
-    const data = await getHistorico();
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-app.post("/rest/jogar", async (req, res) => {
-  try {
-    const data = await postJogada(req.body.jogada);
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// Endpoints SOAP
-app.post("/soap/jogar", async (req, res) => {
-  try {
-    const data = await jogarSOAP(req.body.jogada);
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+const helmet = require('helmet');
+app.use(helmet({
+  contentSecurityPolicy: false
+}));
 
 app.listen(3000, () => {
-  console.log("API Gateway rodando na porta 3000");
+  console.log('API Gateway rodando em http://localhost:3000');
 });
