522148563bc1f96dfadf12fc91a4ea0505141267
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-11-24 09:01:18 +0000

    feat: adicionar rotas e modificar rota inicial

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -3,13 +3,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CriarSala from "./pages/CriarSala";
 import Sala from "./pages/Sala";
+import EntrarSala from "./pages/EntrarSala";
+import Resultado from "./pages/Resultado";
+import Inicio from "./pages/Inicio";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CriarSala />} />
-        <Route path="/sala/:nomeSala" element={<Sala />} />
+        <Route path="/" element={<Inicio />} />
+        <Route path="/criar" element={<CriarSala />} />
+        <Route path="/entrar" element={<EntrarSala/>} />
+        <Route path="/sala/:idSala" element={<Sala />} />
+        <Route path="/resultado" element={<Resultado />} />
       </Routes>
     </Router>
   );
