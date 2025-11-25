ee8ef89bc84ba9beab48ec19fbd9430a23e1927b
Lucas-Tales1 <lucastales100@gmail.com>
2025-11-25 00:44:16 -0300

    feat: implementar HATEOAS

--- rest_service/historico/serializers.py
+++ rest_service/historico/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import Partida
+from rest_framework.reverse import reverse
 
 class PartidaSerializer(serializers.ModelSerializer):
+    links = serializers.SerializerMethodField()
+
     class Meta:
         model = Partida
-        fields = '__all__'
+        fields = ['id', 'jogador1', 'jogador2', 'vencedor', 'data', 'links']
+
+    def get_links(self, obj):
+        request = self.context.get('request') 
+
+        return {
+            'self': reverse('historico-detail', kwargs={'pk': obj.pk}, request=request),
+            'list': reverse('historico-list', request=request)
+        }
