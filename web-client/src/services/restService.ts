98f14ac251ae09d7bfcb62d694941dc0b5b38312
Manoelpj <manoelpinto2005@gmail.com>
2025-11-28 05:53:35 -0300

    feat: permitir acessar o jogo pelo ip

--- web-client/src/services/restService.ts
+++ web-client/src/services/restService.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { type Partida } from "../types";
 
-const API_GATEWAY = "http://localhost:3000/jogo/historico";
+const getApiGatewayUrl = () => {
+  const host = window.location.hostname;
+  return `http://${host}:3000/jogo/historico`;
+};
 
 export const getHistorico = async (): Promise<Partida[]> => {
+  const API_GATEWAY = getApiGatewayUrl();
   const res = await axios.get(API_GATEWAY);
   return res.data.historico || [];
 };
--- web-client/src/services/soapService.ts
+++ web-client/src/services/soapService.ts
@@ -1,19 +1,25 @@
 import axios from "axios";
 
-const API_GATEWAY = "http://localhost:3000/jogo/soap";
+const getApiGatewayUrl = () => {
+  const host = window.location.hostname;
+  return `http://${host}:3000/jogo/soap`;
+};
 
 export const criarSala = async (jogador: string) => {
+  const API_GATEWAY = getApiGatewayUrl();
   const res = await axios.post(`${API_GATEWAY}/criar-sala`, { jogador });
   return res.data.id;
 };
 
 export const entrarSala = async (idSala: number, jogador: string) => {
+  const API_GATEWAY = getApiGatewayUrl();
   const res = await axios.post(`${API_GATEWAY}/entrar-sala`, { idSala, jogador });
   return res.data.sucesso;
 };
 
 export const jogar = async (idSala: number, jogador: string, jogada: string) => {
   try {
+    const API_GATEWAY = getApiGatewayUrl();
     const res = await axios.post(`${API_GATEWAY}/jogar`, { idSala, jogador, jogada });
     return res.data.resultado;
   } catch (error) {
@@ -23,6 +29,7 @@ export const jogar = async (idSala: number, jogador: string, jogada: string) => 
 };
 
 export const verResultado = async (idSala: number) => {
+  const API_GATEWAY = getApiGatewayUrl();
   const res = await axios.post(`${API_GATEWAY}/resultado`, { idSala });
   return res.data.resultado;
 };
