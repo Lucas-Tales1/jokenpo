d9d8741a55d7d0d69f59cbe9c9df57d3e9e53a3a
Lucas Tales <lucastales100@gmail.com>
2025-12-05 13:46:19 +0000

    fix: corrigir ws

--- web-client/src/services/socketService.ts
+++ web-client/src/services/socketService.ts
@@ -1,66 +1,25 @@
 let ws: WebSocket | null = null;
 
-// Função para conectar automaticamente usando o IP/hostname do front-end
 export const conectarWS = () => {
   if (!ws || ws.readyState === WebSocket.CLOSED) {
-    const host = window.location.hostname; 
 
-    ws = new WebSocket(`ws://${host}:3000`);
+    // pega automaticamente o endereço da API Gateway
+    const gatewayWS = window.location.origin.replace("http", "ws");
+
+    ws = new WebSocket(gatewayWS);
 
     ws.onopen = () => {
-      console.log("WebSocket conectado ao servidor:", `ws://${host}:3000`);
+      console.log("WebSocket conectado a:", gatewayWS);
     };
 
-    ws.onerror = (err) => {
-      console.error("Erro no WebSocket:", err);
+    ws.onerror = (e) => {
+      console.error("Erro no WebSocket:", e);
     };
 
     ws.onclose = () => {
-      console.warn("WebSocket foi fechado");
+      console.log("WebSocket desconectado");
     };
   }
 
   return ws;
-};
-
-export const enviarMensagemWS = (
-  sala: string,
-  nomeJogador: string,
-  mensagem: string
-) => {
-  if (ws && ws.readyState === WebSocket.OPEN) {
-    ws.send(
-      JSON.stringify({
-        tipo: "enviar_mensagem",
-        sala,
-        nomeJogador,
-        mensagem,
-        timestamp: new Date().toISOString(),
-      })
-    );
-  } else {
-    console.warn("Não foi possível enviar mensagem: WS não está conectado.");
-  }
-};
-
-export const ouvirMensagensWS = (callback: (dados: any) => void) => {
-  if (!ws) return;
-
-  const listener = (event: MessageEvent) => {
-    try {
-      const data = JSON.parse(event.data);
-      callback(data);
-    } catch (e) {
-      console.error("Erro ao interpretar mensagem WS:", e);
-    }
-  };
-
-  ws.addEventListener("message", listener);
-
-  // retorna a função para remover se quiser
-  return () => ws?.removeEventListener("message", listener);
-};
-
-export const removerOuvidorWS = (callback: any) => {
-  if (ws) ws.removeEventListener("message", callback);
 };
